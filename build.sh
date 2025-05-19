833a95652f803f915baee10fac753406d0c245b5
Lars <larsbeglinger@gmail.com>
2025-05-19 20:03:46 +0200

    including superuser creation

--- build.sh
+++ build.sh
@@ -10,3 +10,17 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser using environment variables if it doesn't exist
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'lars')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'larsbeglinger@gmail.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'changeme0000')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+EOF
