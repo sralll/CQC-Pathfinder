387f3d5b5317c7282508dd928c06131496ff01fc
Lars <larsbeglinger@gmail.com>
2025-05-20 11:41:41 +0200

    unified trailing slash urls for play

--- play/urls.py
+++ play/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='play_home'),
     path('get-files/', views.get_files, name='get_files'),
-    path('load-file/<str:filename>', views.load_file, name='load_file'),
+    path('load-file/<str:filename>/', views.load_file, name='load_file'),
     path('submit_result/', views.submit_result, name='submit_result'),
 ]
