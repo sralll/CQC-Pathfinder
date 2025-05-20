cd64b2c62bc01194baa2556d5f16724911e4e211
Lars <larsbeglinger@gmail.com>
2025-05-20 11:22:52 +0200

    trailing slash added

--- coursesetter/urls.py
+++ coursesetter/urls.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('', views.index, name='coursesetter'),
-    path('get-files', views.get_files, name='get_files'),
+    path('get-files/', views.get_files, name='get_files'),
     path('load-file/<path:filename>/', views.load_file, name='load_file'),
     path('file-exists/<str:filename>/', views.check_file_exists, name='file_exists'),
     path('save-file/', views.save_file, name='save_file'),
