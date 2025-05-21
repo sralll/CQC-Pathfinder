c28f646ba9ab04b9987381a2b7e27a3aabd4f90e
Lars <larsbeglinger@gmail.com>
2025-05-21 18:13:16 +0200

    cleaned start script

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-python manage.py shell << END
+: 'python manage.py shell << END
 print("alive")
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username="lars").exists():
     User.objects.create_superuser("lars", "larsbeglinger@gmail.com", "admin0000")
-END
+END'
 
 gunicorn CQCPathfinder.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
