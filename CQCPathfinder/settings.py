843a7394fff4d79003a61b93e0a385f2f7597123
Lars <larsbeglinger@gmail.com>
2025-05-21 15:47:20 +0200

    Railway added to allowed hosts

--- CQCPathfinder/settings.py
+++ CQCPathfinder/settings.py
@@ -15,10 +15,11 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['cqcpathfinder.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cqcpathfinder.onrender.com', 'cqcpathfinder.up.railway.app/', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://cqcpathfinder.onrender.com',
+    'https://cqcpathfinder.up.railway.app/',
 ]
 
 # Application definition
