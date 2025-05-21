b0476d039c7124b3643333842c811cb491a996ec
Lars <larsbeglinger@gmail.com>
2025-05-21 15:29:36 +0200

    Railways changes

--- build.sh
+++ build.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
-# Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
-
-# Create superuser using environment variables if it doesn't exist
-python manage.py shell <<EOF
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'lars')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'larsbeglinger@gmail.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'changeme0000')
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-EOF
