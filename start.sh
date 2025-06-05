3edac53baa70156f45d641fe4932ad78072fe320
Lars <larsbeglinger@gmail.com>
2025-06-06 00:08:03 +0200

    utc fix

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ User = get_user_model()
 if not User.objects.filter(username="lars").exists():
     User.objects.create_superuser("lars", "larsbeglinger@gmail.com", "admin0000")
 END'
-
+python manage.py makemigrations
+python manage.py migrate
 gunicorn CQCPathfinder.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
