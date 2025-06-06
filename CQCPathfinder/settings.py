7ef1cc74d927e3c14085b8c7a22618765c25d3b2
Lars <larsbeglinger@gmail.com>
2025-06-06 11:10:43 +0200

    UX improvements: loading animations

--- CQCPathfinder/settings.py
+++ CQCPathfinder/settings.py
@@ -6,14 +6,14 @@ from django.core.files.storage import default_storage
 from dotenv import load_dotenv
 
 # Load .env file
-load_dotenv()
+#load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 FILES_DIR = os.path.join(BASE_DIR, 'jsonfiles')
 MAPS_DIR = os.path.join(BASE_DIR, 'maps')
 
-DEBUG = True
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
