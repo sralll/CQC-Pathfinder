f4ed15ac074b6f603f71807ccba85843822202b2
Lars <larsbeglinger@gmail.com>
2026-04-14 21:16:08 +0200

    gunicorn worker tuned for memory leaks

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ gunicorn CQCPathfinder.asgi:application \
   -k uvicorn.workers.UvicornWorker \
   --bind 0.0.0.0:$PORT \
   --workers 4 \
-  --max-requests 5 \
-  --max-requests-jitter 1 \
+  --max-requests 50 \
+  --max-requests-jitter 5 \
   --timeout 30 \
   --graceful-timeout 900 \
   --log-level info
