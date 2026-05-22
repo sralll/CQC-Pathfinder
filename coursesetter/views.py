c7c2311bf866553a532f1b20604099776b58fb1e
Lars <larsbeglinger@gmail.com>
2026-05-22 17:33:40 +0200

    media downloader

--- coursesetter/views.py
+++ coursesetter/views.py
@@ -14,6 +14,42 @@ from .models import publishedFile
 from urllib.parse import unquote
 from accounts.models import UserProfile
 
+
+
+
+import os
+import tarfile
+import tempfile
+from django.conf import settings
+from django.contrib.admin.views.decorators import staff_member_required
+from django.http import StreamingHttpResponse
+
+
+@staff_member_required
+def export_media(request):
+    media_root = settings.MEDIA_ROOT
+
+    def stream_tar(tmp_path):
+        with open(tmp_path, 'rb') as f:
+            while chunk := f.read(8192):
+                yield chunk
+
+    tmp = tempfile.NamedTemporaryFile(delete=False, suffix='.tar.gz')
+    tmp.close()
+
+    try:
+        with tarfile.open(tmp.name, 'w:gz') as tf:
+            tf.add(media_root, arcname='media')
+
+        response = StreamingHttpResponse(stream_tar(tmp.name), content_type='application/gzip')
+        response['Content-Disposition'] = 'attachment; filename="media_export.tar.gz"'
+        response['X-Accel-Buffering'] = 'no'
+        return response
+
+    finally:
+        os.unlink(tmp.name)
+
+
 def get_gamefile_by_public_name(request, filename):
     from .models import publishedFile
     from accounts.models import UserProfile
